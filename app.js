7dc769523e1e8546b8012661c6d293b79938d7ca
Paul Sijpkes <paul.sijpkes@gmail.com>
2019-09-16 11:56:11 +1000

    modified

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ let lrs = new XAPI.LRS();
       return new XAPI.Account(username, //The name of the agent for the Authority
         true,  //this account has read access to the API
         true,   //this account has write access to the API
-        false,   //this account can only retreive data it posted 
-        true   //this account can use the advanced search apis
+        true,   //this account can only retreive data it posted 
+        false   //this account can use the advanced search apis
       ); 
     },
     connectionString: process.env.connectionString || "mongodb://localhost",
@@ -25,29 +25,29 @@ let lrs = new XAPI.LRS();
   //a batch of statements was stored. A single POSTed statement is treated as an array with one item  
   lrs.on("statementStored", function(ids)
   {
-    console.log("The IDs of the stored statements are " + ids.join(","));
-    lrs.getStatement(ids[0]).then(statement => {
-      console.log(statement);
-    });
+      console.log("The IDs of the stored statements are " + ids.join(","));
+      
+      lrs.getStatement(ids[0]).then(statement => {
+          console.log(statement);
+      });
+      
   });
 
   lrs.on("clientError", function(e)
   {
-    console.log("The client sent an invalid xAPI request");
+        console.log("The client sent an invalid xAPI request");
   });
 
-  const serverStmt = {
+  const baseStmt = {
     "id": uuid(),
     "timestamp": new Date().toISOString(),
     "authority": {
-      
         mbox: "mailto:paul.sijpkes@newcastle.edu.au",
         objectType: "Agent"
-      
     },
     "actor": {
       "name": "LRS Server",
-      "mbox": "mailto:paul.sijpkes@newcastle.edu.au"
+      "mbox": "mailto:server@bold.newcastle.edu.au"
     },
     "verb": {
       "id": "http://adlnet.gov/expapi/verbs/experienced",
@@ -62,16 +62,14 @@ let lrs = new XAPI.LRS();
   };
 
   lrs.on('ready', function(){
-    console.log("The lrs is attached to the database and ready. You can now use the programmatic API.")
-    console.log(`HOST: ${process.env.host}`);
-    console.log(`MONGODB: ${process.env.connectionString}`);
+       console.log("The lrs is attached to the database and ready. You can now use the programmatic API.");
+       console.log(`HOST: ${process.env.host}`);
+       console.log(`MONGODB: ${process.env.connectionString}`);
    lrs.insertStatement(serverStmt).then( ()=>{
-      console.log("The statement was stored");
+       console.log("The statement was stored");
     }).catch( (e)=>{
-      console.log("There was some problem with the statement.", e)
+       console.log("There was some problem with the statement.", e)
     });
   });
-
-  
 
 module.exports = app;
